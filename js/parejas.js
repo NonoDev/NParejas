ff17ea66c86a4fd8952e7c681af998e6027ac939
Juan Antonio Valera <j.v.metall@gmail.com>
2014-12-03 01:44:55 +0100

    Añadido botón reiniciar
    Se ha añadido un botón con la funcionalidad de reiniciar el juego desde
    0

--- js/parejas.js
+++ js/parejas.js
@@ -7,6 +7,8 @@ var contadorAciertos = 0;
 var arrayImagenes = ["images/1.jpeg", "images/2.jpeg", "images/3.jpeg", "images/4.jpeg", "images/5.jpg", "images/6.jpg"];
 var cantidadImagenes = arrayImagenes.length;
 var arrayPosiciones = new Array(cantidadImagenes);
+var lista = document.getElementsByTagName('img'); 
+var final = false;
 
 console.log("Variables inicializadas");
 //console.log(cantidadImagenes);
@@ -31,20 +33,20 @@ window.onload = function(){
     addEventos(arrayPosiciones);
 }
 
-function recogerValor(valor, arrayImagenes){
-    console.log("Valor " + valor);
+// Recojo el valor de la imagen en la que se hace click y la cambio para comparar las parejas
+function cambiarImagenes(valor, arrayImagenes){
+//    console.log("Valor " + valor);
     contadorPruebas++;
     var casillaID = document.getElementById(valor).id;
     var casillaIMG = document.getElementById(valor);
-//    console.log(casillaID, casillaIMG);
-//    console.log('Anterior: ' + anterior);
-//    console.log('CasillaID: ' + casillaID);
-    
-            
+
+    // Si ya se ha hecho click en una imagen, compruebo la segunda
     if(contadorPruebas>1){
         imagenElegida = arrayPosiciones[casillaID];
         var anteriorIMG = document.getElementById(anterior);
         casillaIMG.setAttribute('src', arrayImagenes[imagenElegida]);
+        casillaIMG.setAttribute('name', 'cambiada');
+        // Si es diferente a la anterior, vuelvo a poner la imagen inicial tras 1 segundo
         if(arrayPosiciones[casillaID]!=arrayPosiciones[anterior]){
             contadorFallos++;   
             window.setTimeout(function(){
@@ -55,22 +57,48 @@ function recogerValor(valor, arrayImagenes){
             contadorAciertos++;   
         }
         contadorPruebas = 0;
+    // Si es igual, las dejo cambiadas con la nueva imagen
     }else{
         anterior = casillaID;
         imagenElegida = arrayPosiciones[casillaID];
         casillaIMG.setAttribute('src', arrayImagenes[imagenElegida]);
+        casillaIMG.setAttribute('name', 'cambiada');
         //console.log('AnteriorIMG: ' + anteriorIMG);
         contadorPruebas++;
     }
+    terminar();
 }
 
+// Añado los eventos onclick a todas las imagenes
 function addEventos(listaId){
-    var lista = document.getElementsByTagName('img');
-    for (i = 0; i < lista.length; i++){
-        lista[i].addEventListener('click', function(){recogerValor(this.id, arrayImagenes)});    
+    for (var i = 0; i < lista.length; i++){
+        lista[i].addEventListener('click', function(){cambiarImagenes(this.id, arrayImagenes)});    
     }
-    
-    //console.log(lista);
-    //console.log(listaId);
+    var btnreiniciar = document.getElementById('reiniciar');
+    btnreiniciar.addEventListener('click', function(){reiniciar()});
 }
 
+// Funcion que comprueba que se ha terminado el juego
+function terminar(){
+    for(var x = 0; x < lista.length; x++){
+        
+//        if(lista[x].name == 'cambiada' && lista[x].name != ''){
+//            //final = true;
+//            console.log('No fin');
+//        }else{
+//            //final = false; 
+//           // alert('Fin');
+//            console.log('fin');
+//        }
+        //console.log(lista[0].src);
+}
+}
+
+// Funcion que reinicia el juego
+function reiniciar(){
+    for(var i = 0; i < lista.length; i++){
+        lista[i].setAttribute('src', 'images/0.jpg');   
+        contadorAciertos = 0;
+        contadorFallos = 0;
+    }
+}
